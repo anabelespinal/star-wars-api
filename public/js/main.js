a93788e12b4635179901b29f15e945125e91387d
anbelespinal <anabelespinal>
2016-11-11 19:36:12 -0500

    avanzando

--- public/js/main.js
+++ public/js/main.js
@@ -31,65 +31,84 @@ var peopleSpecie = '<div class="col s12 m6">'+
 						        '</div>';
 
 $(document).ready(function(){
-// species
-	var optionSpecies = function(response){
-		var people="";
-		$.each(response.results, function(i, peoples){
-			people += select
-				.replace("{{species}}", peoples.name)
-				.replace("{{url-species}}", peoples.people);
-		});
-		$("#species").html('<option value="" disabled selected>SPECIES</option>');
-		$("#species").append(people);
-		$("#species").change(function(e) {
-			var optionArray = $("option");
-			for (var i = 1; i < optionArray.length; i++){
-				var data = optionArray[i].getAttribute("data-species-url");
-				var split = data.split(",");
-				for (var j=0; j<split.length;j++){
-					var g = split[j];
-					$.getJSON(g, function(response){
-						var per="";
-						per += peopleSpecie.replace("{{personajes}}", response.name);
-						$("#people-species").append(per);
+	var optionSpecies = function(respuestaEspecie){
+		var tipoSpecie="";
+		$.each(respuestaEspecie.results, function(i, nombredeEspecies){
+			tipoSpecie += select
+				.replace("{{species}}", nombredeEspecies.name)
+				.replace("{{url-species}}", nombredeEspecies.people);
+				// console.log(nombredeEspecies.people)
+			$("#species").html('<option value="" disabled selected>SPECIES</option>');
+			$("#species").append(tipoSpecie);
+
+			var elementOption = $("optionn");
+			$.each(elementOption, function(i, atributoUrl){
+				var urls = $(atributoUrl).attr("data-species-url");
+				var splitUrls = urls.split(",");
+				$("#select").change(function(){
+					$.each(splitUrls, function(i, url){
+						$.getJSON(url, function(response){
+							var div = "";
+								if(response.species == respuestaEspecie.url){
+									div += peopleSpecie.replace("{{personajes}}", response.name);
+									$("#people-species").append()
+							}
+						})
 					});
-				}
-			}
+				});
+			});
+
+
 		});
-	}
-	$.getJSON("http://swapi.co/api/species/", optionSpecies);
 
 
 
-	// persons 
-		// 	var optionArray = $("option");
+					// $.getJSON(url, function(response){
+					// 	var div = "";
+					// 	$("#select").change(function(){
+					// 		if(response.species == respuestaEspecie.url){
+					// 			div += peopleSpecie.replace("{{personajes}}", response.name);
+					// 			$("#people-species").append()
+					// 		}
+					// 	})
+					// });
 
-		// for (var i = 1; i< optionArray.length; i++){
-		// 	$(optionArray[i]).change(mostrarDatos);
-		// 	// var dataSpeciesUrl = optionArray[i].getAttribute("data-species-url")
-		// }
-		// function mostrarDatos(){
-		// 	var dataSpeciesUrl = optionArray[i].getAttribute("data-species-url").split(",");
-		// 	for (var j = 0; j< dataSpeciesUrl.length;j++) {
-		// 		var sumar = dataSpeciesUrl[j].slice(27,29);
-		// 		var indice = sumar[0] + sumar[1];
 
-		// 		var data = "http://swapi.co/api/people/" + indice + "/";
-		// 		$.getJSON(data, function(response){
-		// 			var per="";
-		// 			per += peopleSpecie.replace("{{personajes}}", response.name);
-		// 		});
-		// 		$("#people-species").append(per);
+
+		// var elementOption  =$("option");
+
+		// 	for (var i = 1; i < elementOption.length; i++){
+		// 		var urls = $(elementOption[i]).attr("data-species-url");
+		// 		var splitUrls = urls.split(",");
+		// 			$.getJSON("http://swapi.co/api/people/", function(respuestaPersona){
+		// 				for(var a = 0; a< splitUrls.length;a++){
+		// 					var nombrePerson = "";
+		// 					$.each(respuestaPersona.results, function(i, speciesUrl){
+		// 						$("#select").change(function(){
+		// 							if( speciesUrl.species == nombredeEspecies.url){
+		// 								nombrePerson += peopleSpecie.replace("{{personajes}}", speciesUrl.name);
+		// 								$("#people-species").append(nombrePerson);
+		// 							}
+		// 						});
+		// 					});
+		// 				}
+		// 			});
 		// 	}
-		// 	// var indice = dataSpeciesUrl[0] + dataSpeciesUrl[1];
-		// }
 
 
+
+		// $("#species").change(ff);
+	}
+	var s_p = "http://swapi.co/api/species/";
+	$.getJSON(s_p, optionSpecies);
+});
 
 
 
 
 // personajes
+
+$(document).ready(function(){
 	var formatResponse= function(response){	
 		$("#total").text(response.results.length);
 		var personajes="";
@@ -133,22 +152,105 @@ $(document).ready(function(){
 
 
 
-		// $("#species").change(function(e) {
+
+
+
+
+// mio
+
+	// var optionSpecies = function(response){
+	// 	var people="";
+	// 	$.each(response.results, function(i, peoples){
+	// 		people += select
+	// 			.replace("{{species}}", peoples.name)
+	// 			.replace("{{url-species}}", peoples.people);
+	// 	});
+	// 	$("#species").html('<option value="" disabled selected>SPECIES</option>');
+	// 	$("#species").append(people);
+	// 	$("#species").change(function(e) {
+	// 		var optionArray = $("option");
+	// 		for (var i = 1; i < optionArray.length; i++){
+	// 			var data = optionArray[i].getAttribute("data-species-url");
+	// 			var split = data.split(",");
+	// 			for (var j=0; j<split.length;j++){
+	// 				var urlsSpecies = split[j];
+	// 				$.getJSON(urlsSpecies, function(response){
+	// 					var per="";
+	// 					per += peopleSpecie.replace("{{personajes}}", response.name);
+	// 					$("#people-species").append(per);
+	// 				});
+	// 			}
+	// 		}
+	// 	});
+	// }
+	// $.getJSON("http://swapi.co/api/species/", optionSpecies);
+
+
+
+
+
+	// 2
+// 	$(document).ready(function(){
+// 	var optionSpecies= function(response){
+// 		var people = "";
+// 		$.each(response.results, function(i, personajes){
+// 			var dataUrl= "";
+// 			var peopleUrl = "http://swapi.co/api/people/";
+// 			$.each(personajes.people, function(i, url){
+// 				console.log(url);
+// 				dataUrl += url.replace(peopleUrl , "");
+// 			});
+// 			people += select
+// 				.replace("{{species}}", personajes.name)
+// 				.replace("{{data-species-url}}", dataUrl.substring(0, dataUrl.length-1));
+// 		});
+// 		$("#species").html('<option value="" disabled selected>SPECIES</option>');
+// 		$("#species").append(people);
+// 	}
+// 	$.getJSON("http://swapi.co/api/species/", optionSpecies);
+// });
+
+// $(".row").on("change", "#species", function(){
+// 	$("#people-species").html("");
+// 	var numero = $(this).val().split("/");
+// 	for (var i = 0; i<numero.length; i++){
+// 		$.getJSON("http://swapi.co/api/people/"+numero[i]+"/", function(response){
+// 			var tipoSpecie = peopleSpecie.replace("{{personajes}}", response.name);
+// 			$("#people-species").append(tipoSpecie);
+// 		});
+// 	}
+// });
+
+
+
+
+
+
+// ultimo
+
+
+		// var ff= function(){
 		// 	var optionArray = $("option");
 		// 	for (var i = 1; i < optionArray.length; i++){
 		// 		var data = optionArray[i].getAttribute("data-species-url");
 		// 		var split = data.split(",");
 		// 		for (var j=0; j<split.length;j++){
+		// 			var urlsSpecies = split[j];
+		// 			$.getJSON(urlsSpecies, function(responseDos){
+		// 				var per="";
+		// 				per += peopleSpecie.replace("{{personajes}}", responseDos.name);
+		// 				// $("#people-species").append(per);
+		// 				$.getJSON(s_p, function(response){
+		// 					$.each(response.results, function(i, ggg){
+		// 						var hh = ggg.url;
+		// 						var dd = responseDos.species;
 
+		// 						if(hh == dd){
+		// 							$("#people-species").append(per);
+		// 						}
+		// 					});
+		// 				});
+		// 			});
 		// 		}
-		// 		$.getJSON(data, function(response){
-		// 			var per="";
-		// 			per += peopleSpecie.replace("{{personajes}}", response.name);
-		// 			var a = "http://swapi.co/api/species/" + optionArray[i] + "/";
-		// 			// if(response.species == a){
-		// 			// 	$("#people-species").append(per);
-		// 			// }
-		// 			$("#people-species").append(per);
-		// 		})
 		// 	}
-		// });
+		// }
